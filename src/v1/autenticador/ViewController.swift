c2d4a5b38029d996966c2eeee9b268dbe4924635
Luiz Antônio Jr <silvajuniorluizantonio@gmail.com>
2021-01-04 22:45:09 -0300

    init

---
+++ src/v1/autenticador/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autenticador
+//
+//  Created by Luiz Antônio da Silva Júnior on 04/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
