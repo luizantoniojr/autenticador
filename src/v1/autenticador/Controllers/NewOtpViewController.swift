b6051d433c6d01d4ee2699603bd345af22cbc4fb
Luiz Antônio Jr <silvajuniorluizantonio@gmail.com>
2021-01-06 00:24:12 -0300

    Add New Otp View Controller

---
+++ src/v1/autenticador/Controllers/NewOtpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewOtpViewController.swift
+//  autenticador
+//
+//  Created by Luiz Antônio da Silva Júnior on 05/01/21.
+//
+
+import Foundation
+import UIKit
+
+class NewOtpViewController:  UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(
+            title: "Voltar", style: .plain, target: nil, action: nil)
+    }
+}
